ba40f82c711dbe922730f6f94dabf556bbd47d74
EdwinEngelbrethsen <edwin_engelbrethsen@outlook.com>
2024-02-24 20:53:14 +0100

    update repo

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts,js,jsx,tsx,css,scss,sass}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
